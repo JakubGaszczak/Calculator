87b2859d439acbc90c3b2ec2d7f1e79a1006771b
JakubGaszczak <jakub.gaszczak2000@gmail.com>
2022-06-15 00:51:04 +0200

    Dodanie funkcji sprawdzającej operator i wykonującej obliczenia oraz implementacja kilku event listenerów

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
-const equalsButtons = document.querySelector("[data-equlas]");
-const deleteButtons = document.querySelector("[data-delete]");
-const clearButtons = document.querySelector("[data-clear]");
+const equal = document.querySelector(".equal");
+const del = document.querySelector(".delete-btn");
+const clear = document.querySelector(".clear-btn");
 const currentDisplayNumber = document.querySelector("[data-current-operand]");
 const previousDisplayNumber = document.querySelector("[data-previous-operand]");
 
@@ -41,6 +41,44 @@ function handleOperator(op) {
     currentNumber = "";
     currentDisplayNumber.textContent = ""; 
 }
+
+//funtcion that check selected operator and performs calculations
+function calculate() {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber); 
+
+    if (operator == "+") {
+        previousNumber = previousNumber + currentNumber;
+    } else if (operator == "-") {
+        previousNumber = previousNumber - currentNumber;
+    } else if (operator == "/") {
+        previousNumber = previousNumber / currentNumber;
+    } else if (operator == "*") {
+        previousNumber = previousNumber * currentNumber;
+    }
+    previousDisplayNumber.textContent = "";
+    currentDisplayNumber.textContent = previousNumber;
+}
+
+
+
+//event listeners
+equal.addEventListener("click", calculate);
+
+del.addEventListener("click", () => {
+    currentNumber = currentNumber.slice(0, -1);
+    currentDisplayNumber.textContent = currentNumber;
+})
+
+clear.addEventListener("click", () => {
+    currentDisplayNumber.textContent = "";
+    previousDisplayNumber.textContent = "";
+    currentNumber = "";
+    previousNumber = "";
+    operator = "";
+})
+
+
 
 
 
